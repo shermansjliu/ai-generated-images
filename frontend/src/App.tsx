8eb41d27411506d46a4bd47cd103e9ae93f7bad7
Sherman Liu <sherman.liu@mail.utoronto.ca>
2023-01-02 11:26:09 +0800

    switched to ccr with create-react-app and chakra ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <h1>Hello world</h1>
+  </ChakraProvider>
+)
